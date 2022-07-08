c06ba2ef786192f58fb93f2a1f1d9479c9b48a98
JEAR2 <jhedacro@gmail.com>
2022-07-08 18:37:25 -0500

    Inicio de la actividad correcion size de las pruebas

---
+++ src/main/java/katas/Kata6.java
@@ -0,0 +1,21 @@
+package katas;
+
+import model.BoxArt;
+import model.Movie;
+import util.DataUtil;
+
+import java.util.List;
+import java.util.Optional;
+
+/*
+    Goal: Retrieve the url of the largest boxart using map() and reduce()
+    DataSource: DataUtil.getMovieLists()
+    Output: String
+*/
+public class Kata6 {
+    public static String execute() {
+        List<Movie> movies = DataUtil.getMovies();
+
+        return "someUrl";
+    }
+}
